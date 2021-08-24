4e9338efb714084c1c91c1fbac1f5d9afb8dd35d
Toprak Yilmaz <67636033+toprk@users.noreply.github.com>
2021-08-24 17:33:23 +0300

    Update poll.py

--- poll.py
+++ poll.py
@@ -4,23 +4,20 @@ class Poll:
 
     _choice_dict = {}
     _vote_count = {}
-    _index = 0
-    _MAX_OPTION = 0
-    _default_s = 0
-    _title = ""
     _total_vote_count = 0
 
-    def __init__(self, title="Default Title", MAX_OPTION=5, default_s=4):
-        self._title = title
-        self._MAX_OPTION = MAX_OPTION
-        self._default_s = default_s
+    def __init__(self, title: str, MAX_OPTION=5, default_s=4):
+        self.title = title
+        self.MAX_OPTION = MAX_OPTION
+        self.default_s = default_s
 
     def add_options(self):
 
+        __index = 0
         while True:
-            choice = input(f"\nAdd an Option (MIN 2, MAX {self._MAX_OPTION}) (Type done/DONE to complete the Addition part): ")
+            choice = input(f"\nAdd an Option (MIN 2, MAX {self.MAX_OPTION}) (Type done/DONE to complete the Addition part): ")
 
-            if choice in (“done”,”DONE”):
+            if choice in ("done","DONE"):
                 if len(self._choice_dict) < 2:
                     print("You can add MIN 2 options.")
                     continue
@@ -31,12 +28,12 @@ class Poll:
                 print("\nYou cannot use BLANK name.")
                 continue
 
-            self._choice_dict[self._index] = choice
-            self._vote_count[self._index] = 0
-            self._index += 1
+            self._choice_dict[__index] = choice
+            self._vote_count[__index] = 0
+            __index += 1
 
-            if len(self._choice_dict) > self._MAX_OPTION - 1:
-                    print(f"You can add MAX {self._MAX_OPTION} Options.\n")
+            if len(self._choice_dict) > self.MAX_OPTION - 1:
+                    print(f"You can add MAX {self.MAX_OPTION} Options.\n")
                     return True
 
                 
@@ -44,23 +41,23 @@ class Poll:
         return round((self._vote_count[n]/self._total_vote_count)*100)
 
 
-    def _print_poll(self, title, ended=False):
+    def _print_poll(self, ended=False):
         longest_string = max(self._choice_dict.values(), key=len)
 
         print("\n  ------------------------------------")
-        print(f"    {title}")
+        print(f"    {self.title}")
         print("  ------------------------------------")
         
         for i in range(len(self._choice_dict)):
-            s = len(longest_string) + self._default_s - len(self._choice_dict[i])
+            s = len(longest_string) + self.default_s - len(self._choice_dict[i])
 
             try:
 
                 print("  ------------------------------------")
-                print(" "*self._default_s, i+1,  self._choice_dict[i], " "*s + "|", self._vote_count[i], "|" + " "*self._default_s + "%", self._calc_p(i))
+                print(" "*self.default_s, i+1,  self._choice_dict[i], " "*s + "|", self._vote_count[i], "|" + " "*self.default_s + "%", self._calc_p(i))
                 print("  ------------------------------------")           
             except ZeroDivisionError:
-                print(" "*self._default_s, i+1, self._choice_dict[i], " "*s  + "|", self._vote_count[i], "|" + " "*self._default_s + "%0")
+                print(" "*self.default_s, i+1, self._choice_dict[i], " "*s  + "|", self._vote_count[i], "|" + " "*self.default_s + "%0")
                 print("  ------------------------------------")
 
         if ended:
@@ -73,14 +70,14 @@ class Poll:
 
         while True:
 
-            self._print_poll(self._title)
+            self._print_poll()
             vote_index = input("Please select who to vote for ('Q' to Quit): ")
 
             try:
                 int(vote_index)
             except ValueError:
                 if vote_index == "Q":
-                    self._print_poll(self._title, ended=True)
+                    self._print_poll(ended=True)
                     quit()
                 else:
                     print(f"\nPlease use numbers between 1-{len(self._choice_dict)}")
